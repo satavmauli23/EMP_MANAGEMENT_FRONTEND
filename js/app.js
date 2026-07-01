fbead398b6d9cf3d17b9151ed2179c09a33d5dfc
satavmauli23 <satavmauli23@gmail.com>
2026-07-01 14:44:30 +0530

    Initial frontend commit

---
+++ js/app.js
@@ -0,0 +1,92 @@
+const button = document.getElementById("searchBtn");
+
+const report = document.getElementById("downloadReport");
+
+button.addEventListener("click", loadEmployees);
+
+report.addEventListener("click",downloadReport)
+
+async function loadEmployees() {
+
+    const deptId = document.getElementById("departmentId").value;
+
+    if (!deptId) {
+        alert("Enter Department ID");
+        return;
+    }
+
+    const response = await fetch(
+        `${BASE_URL}/employees/departments/${deptId}`
+    );
+
+    if (!response.ok) {
+    const error = await response.json();
+    alert(error.message);
+    return;
+   }
+
+    const employees = await response.json();
+    if (employees.length === 0) {
+        alert("No employees in this department");
+        document.querySelector("#employeeTable tbody").innerHTML = "";
+        return;
+    }
+
+    const tbody = document.querySelector("#employeeTable tbody");
+
+    tbody.innerHTML = "";
+
+    employees.forEach(emp => {
+
+        tbody.innerHTML += `
+
+<tr onclick="openDetails(${emp.id})">
+
+<td>${emp.id}</td>
+
+<td>${emp.name}</td>
+
+<td>${emp.email}</td>
+
+<td>${emp.position}</td>
+
+<td>${emp.salary}</td>
+
+</tr>
+
+`;
+
+    });
+
+}
+
+async function downloadReport() {
+    try {
+        const response = await fetch(`${BASE_URL}/pdf/report`);
+
+        if (!response.ok) {
+            const error = await response.json();
+            alert(error.message);
+            return;
+        }
+        const blob = await response.blob();
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "report.pdf";
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+
+    } catch (error) {
+        console.error("Download failed:", error);
+        alert("Failed to download report.");
+    }
+}
+
+function openDetails(id){
+
+    window.location.href=`details.html?id=${id}`;
+
+}
---
+++ js/details.js
@@ -0,0 +1,99 @@
+const params = new URLSearchParams(window.location.search);
+
+const employeeId = params.get("id");
+
+loadEmployee();
+
+async function loadEmployee(){
+
+const response = await fetch(
+
+`${BASE_URL}/employees`
+
+);
+
+const employees = await response.json();
+
+const employee = employees.find(e=>e.id==employeeId);
+
+document.getElementById("name").value=employee.name;
+document.getElementById("email").value=employee.email;
+document.getElementById("position").value=employee.position;
+document.getElementById("salary").value=employee.salary;
+
+}
+
+document.getElementById("updateBtn")
+.addEventListener("click",updateEmployee);
+
+async function updateEmployee() {
+
+const employee={
+
+name:document.getElementById("name").value,
+email:document.getElementById("email").value,
+position:document.getElementById("position").value,
+salary:document.getElementById("salary").value
+};
+
+const response = await fetch(
+
+`${BASE_URL}/employees/${employeeId}`,
+
+{
+
+
+method:"PATCH",
+
+headers:{
+
+"Content-Type":"application/json"
+
+},
+
+body:JSON.stringify(employee)
+
+}
+
+);
+  if (!response.ok) {
+    const error = await response.json();
+    alert(error.message);
+    return;
+   }
+
+alert("Updated");
+
+}
+
+document.getElementById("deleteBtn")
+.addEventListener("click",deleteEmployee);
+
+async function deleteEmployee(){
+
+const deptId=prompt("Department Id");
+
+const response = await fetch(
+
+`${BASE_URL}/employees/departments/${deptId}/${employeeId}`,
+
+{
+
+method:"DELETE"
+
+}
+
+);
+
+
+  if (!response.ok) {
+    const error = await response.json();
+    alert(error.message);
+    return;
+   }
+
+alert("Deleted");
+
+window.location="index.html";
+
+}
